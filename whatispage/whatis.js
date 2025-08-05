4191324f225a6b38398b059462bf667bb1560bd8
Winter Falls <178199047+FallenWinters@users.noreply.github.com>
2025-08-05 04:24:12 -0400

    started on movement code for game (might change the game tho)

--- whatispage/whatis.js
+++ whatispage/whatis.js
@@ -1,1 +1,43 @@
 //Imma make a game here so beware of change >:D
+const player_chara = document.getElementById('crow_cara');
+const moveAmount = 5;
+let x = 0;
+let y = 0;
+
+document.addEventListener("keyup", event =>{
+    player_chara.src = null;
+})
+
+document.addEventListener("keydown", event =>{
+    if(event.key.toLowerCase('a') || event.key.toLowerCase('d') ||  event.key.toLowerCase('w') 
+        || event.key.startsWith('Arrow') || event.key.toLowerCase('spacebar')){
+    
+        switch(event.key){
+            case 'ArrowLeft':
+                x-=moveAmount;
+                break;
+            case 'ArrowRight':
+                x+=moveAmount;
+                break;
+            case 'ArrowUp':
+                y-=moveAmount;
+                break;
+            case 'a':
+                x-=moveAmount;
+                break;
+            case 'd':
+                x+=moveAmount;
+                break;
+            case 'w':
+                y-=moveAmount;
+                break;
+            case 'spacebar':
+                y-=moveAmount;
+                break;
+            
+        }
+    
+    player_chara.style.top = `${y}px`;
+    player_chara.style.left = `${x}px`;
+    }
+})
